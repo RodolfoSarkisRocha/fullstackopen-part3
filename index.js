580841522839907df23c153684422feb8d347c96
Rodolfo Sarkis Rocha <rodolfosarkis.r@gmail.com>
2020-07-21 21:44:39 -0300

    Exercises 3.1-3.9

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require('cors')
 const { token } = require("morgan");
 
 app.use(express.json());
+app.use(cors());
 
 token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
@@ -102,7 +104,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
